ca71767d87bdc5038960dc383145ac181697c5f4
JahirBautistaQ <02210132008@mail.udes.edu.co>
2026-04-05 15:32:54 -0500

    Paso 4: implementación de notificadores concretos (Email, SMS, Push)

--- creational-patterns/src/main/java/com/patrones/u2/App.java
+++ creational-patterns/src/main/java/com/patrones/u2/App.java
@@ -10,12 +10,31 @@ package com.patrones.u2;
 
 public class App {
     public static void main(String[] args) {
+
+        Notifier email = new EmailNotifier();
+        Notifier sms = new SmsNotifier();
+        Notifier push = new PushNotifier();
+
+        email.send("correo@gmail.com", "Hola Email");
+        sms.send("3001234567", "Hola SMS");
+        push.send("user123", "Hola Push");
+
+        NotificationLogger.INSTANCE.printAll();
+    }
+}
+
+/*
+Prueba 2
+
+public class App {
+    public static void main(String[] args) {
         NotificationLogger.INSTANCE.log("EMAIL", "Jahir", "Hola mundo");
         NotificationLogger.INSTANCE.log("SMS", "Cliente", "Tu pedido fue enviado");
 
         NotificationLogger.INSTANCE.printAll();
     }
 }
+*/
 
 /*public class App 
 {
