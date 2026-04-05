4dd3df356b13dd01ba2e99ad8cd207e55fb7db24
JahirBautistaQ <02210132008@mail.udes.edu.co>
2026-04-05 15:23:19 -0500

    Paso 2: implementación del patrón Singleton con NotificationLogger

--- creational-patterns/src/main/java/com/patrones/u2/App.java
+++ creational-patterns/src/main/java/com/patrones/u2/App.java
@@ -4,10 +4,24 @@ package com.patrones.u2;
  * Hello world!
  *
  */
-public class App 
+
+
+
+
+public class App {
+    public static void main(String[] args) {
+        NotificationLogger.INSTANCE.log("EMAIL", "Jahir", "Hola mundo");
+        NotificationLogger.INSTANCE.log("SMS", "Cliente", "Tu pedido fue enviado");
+
+        NotificationLogger.INSTANCE.printAll();
+    }
+}
+
+/*public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
     }
 }
+*/
